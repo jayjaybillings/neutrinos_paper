c379b1a3f2415e509cefe571522415bc52c7111d
Jay Jay Billings <jjbillin@amazon.com>
2022-07-12 20:17:37 -0400

    Finished gnuplot comparison plotting and testing function.

--- src/neutrinos.jl
+++ src/neutrinos.jl
@@ -12,32 +12,35 @@ using PlotlyJS
 """
 Fermi's weak coupling constant in MeV cm^2
 """
-const fermiConst = 8.955E-44;
+const fermiConst = 8.955E-44
 
 """
 The difference in neutrino masses of types one and two in MeV^2
 """
-const deltaMSquared = 7.5E-17;
+const deltaMSquared = 7.5E-17
 
 """
 The energy of the neutrinos in MeV
 """
-const energy = 10.0;
+const energy = 10.0
 
 """
 This function computes the matter mixing angle (theta_m) based on the vacuum mixing angle (theta) and the coupling strength (chi).
 """
 function getMatterMixingAngle(vacuumMixingAngle, couplingStrength)
 
-    twoTheta = 2.0*vacuumMixingAngle;
-    cosTwoTheta = cos(twoTheta);
-    matterMixingAngle = 0.5*atan(sin(twoTheta)/(cos(twoTheta) - couplingStrength));
-    scalingFactor = getScalingFactor(vacuumMixingAngle,couplingStrength);
-    println("2T = $twoTheta, chi = $couplingStrength, cosT2M = $cosTwoTheta, theta_m = $matterMixingAngle, scalingFactor = $scalingFactor");
+    twoTheta = 2.0 * vacuumMixingAngle
+    cosTwoTheta = cos(twoTheta)
+    matterMixingAngle =
+        0.5 * atan(sin(twoTheta) / (cos(twoTheta) - couplingStrength))
+    scalingFactor = getScalingFactor(vacuumMixingAngle, couplingStrength)
+    println(
+        "2T = $twoTheta, chi = $couplingStrength, cosT2M = $cosTwoTheta, theta_m = $matterMixingAngle, scalingFactor = $scalingFactor",
+    )
 
-    matterMixingAngle = 0.5*asin(sin(twoTheta)/scalingFactor);
+    matterMixingAngle = 0.5 * asin(sin(twoTheta) / scalingFactor)
 
-    return matterMixingAngle;
+    return matterMixingAngle
 end
 
 """
@@ -45,18 +48,18 @@ This function computes the coupling strength (chi) from the neutrino energy (E) 
 """
 function getCouplingStrength(energy, e_number_density)
 
-    k = ((2.0*sqrt(2.0)*energy*fermiConst)/deltaMSquared);
-    println("$k");
-    coupling_strength = k*e_number_density;
+    k = ((2.0 * sqrt(2.0) * energy * fermiConst) / deltaMSquared)
+    println("$k")
+    coupling_strength = k * e_number_density
 
-    return coupling_strength;
+    return coupling_strength
 end
 
 """
 This function computes the electron number density as a function of the radial fraction, R/R_sun.
 """
 function getElectronNumberDensity(radiusFraction)
-    return 1.475E26*exp(-10.54*radiusFraction);
+    return 1.475E26 * exp(-10.54 * radiusFraction)
 end
 
 """
@@ -64,17 +67,18 @@ This function computes the probability of a neutrino to be in the electron neutr
 """
 function getElectronNeutrinoProbability(radiusFraction, theta)
     # Compute the number density
-    electronNumberDensity = getElectronNumberDensity(radiusFraction);
+    electronNumberDensity = getElectronNumberDensity(radiusFraction)
     # Compute the coupling strength
-    couplingStrength = getCouplingStrength(energy, electronNumberDensity);
+    couplingStrength = getCouplingStrength(energy, electronNumberDensity)
     # Compute the matter mixing angle
-    matterMixingAngle = getMatterMixingAngle(theta, couplingStrength);
-    println("$electronNumberDensity, $couplingStrength, $matterMixingAngle");
+    matterMixingAngle = getMatterMixingAngle(theta, couplingStrength)
+    println("$electronNumberDensity, $couplingStrength, $matterMixingAngle")
 
     # Compute the probability
-    probability = 0.5*(1.0 + (cos(2.0*theta) * cos(2.0*matterMixingAngle)));
+    probability =
+        0.5 * (1.0 + (cos(2.0 * theta) * cos(2.0 * matterMixingAngle)))
 
-    return probability;
+    return probability
 end
 
 """
@@ -82,28 +86,31 @@ This function computes the probability of a neutrino to be in the muon neutrino 
 """
 function getMuonNeutrinoProbability(radiusFraction, mixingAngle)
     # This is just the opposite of the electron neutrino probability.
-    return 1.0 - getElectronNeutrinoProbability(radiusFraction, mixingAngle);
+    return 1.0 - getElectronNeutrinoProbability(radiusFraction, mixingAngle)
 end
 
 """
 This function returns the scaling factor f(theta,chi) for oscillations in matter where theta is the vacuum mixing angle and chi is the coupling strength.
 """
 function getScalingFactor(theta, couplingStrength)
-    return sqrt(1.0-2.0*couplingStrength*cos(2.0*theta) + couplingStrength*couplingStrength);
+    return sqrt(
+        1.0 - 2.0 * couplingStrength * cos(2.0 * theta) +
+        couplingStrength * couplingStrength,
+    )
 end
 
 """
 This function plots the electron number density as a function of the the radial fraction, R/R_sun.
 """
 function plotElectronNumberDensity()
-    n = 100;
-    xValues = 0.0:0.01:n;
-    yValues = Array{Float32}(undef,n);
+    n = 100
+    xValues = 0.0:0.01:n
+    yValues = Array{Float32}(undef, n)
     for i = 1:n
-        yValues[i] = log10(getElectronNumberDensity(xValues[i]));
+        yValues[i] = log10(getElectronNumberDensity(xValues[i]))
     end
-    layout = Layout(;title="Electron Number Density");
-    plot(xValues,yValues,layout);
+    layout = Layout(; title = "Electron Number Density")
+    plot(xValues, yValues, layout)
 
 end
 
@@ -111,28 +118,30 @@ end
 This function plots the scaling factor f(theta,chi).
 """
 function plotScalingFactor(theta)
-    n = 400;
-    xValues = 0.0:0.01:n;
-    yValues = Array{Float32}(undef,n);
+    n = 400
+    xValues = 0.0:0.01:n
+    yValues = Array{Float32}(undef, n)
     for i = 1:n
-        yValues[i] = getMatterMixingAngle(theta, xValues[i]);
+        yValues[i] = getMatterMixingAngle(theta, xValues[i])
     end
-    layout = Layout(;title="Scaling Factor");
-    plot(xValues,yValues,layout);
+    layout = Layout(; title = "Scaling Factor")
+    plot(xValues, yValues, layout)
 end
 
 """
 This function plots the matter mixing angle as function of vacuum mixing angle and coupling strength.
 """
 function plotMatterMixingAngle(theta)
-    n = 400;
-    xValues = 0.0:0.01:n;
-    yValues = Array{Float32}(undef,n);
+    n = 1000
+    xValues = collect(range(0.01,0.6,n))
+    yValues = Array{Float32}(undef, n)
     for i = 1:n
-        yValues[i] = (xValues[i] > 1.0)  ? getMatterMixingAngle(theta, xValues[i]) : pi/2.0 - getMatterMixingAngle(theta, xValues[i]);
+        yValues[i] =
+            (xValues[i] > 1.0) ? getMatterMixingAngle(theta, xValues[i]) :
+            pi / 2.0 - getMatterMixingAngle(theta, xValues[i])
     end
-    layout = Layout(;title="Matter Mixing Angle");
-    plot(xValues,yValues,layout);
+    layout = Layout(; title = "Matter Mixing Angle")
+    plot(xValues, yValues, layout)
 end
 
 """
@@ -144,74 +153,70 @@ function plotMatterMixingAngleLikeGnuplot()
     # using realistic density profile parameterized from
     # Standard Solar Model results from Bahcall
 
-    println("Testing plotting like gnuplot");
+    println("Testing plotting like gnuplot")
 
     # Input data
 
-    radcon = 180.0/pi;  # Radian-degrees conversion
+    radcon = 180.0 / pi  # Radian-degrees conversion
 
-    theta_v = 35.0 ;         # Vacuum mixing angle, degrees
-    tv = theta_v/radcon;     # Convert to radians
-    E = 10.0;       # Neutrino energy in MeV
-    dm2 = 7.6e-5;            # Difference in mass squared in eV^2
-    m12 = 5e-5;           # mass-squared of nu_1 (assumed)
-    m22 = m12+dm2;         # mass-squared of nu_2
+    theta_v = 35.0         # Vacuum mixing angle, degrees
+    tv = theta_v / radcon     # Convert to radians
+    E = 10.0       # Neutrino energy in MeV
+    dm2 = 7.6e-5            # Difference in mass squared in eV^2
+    m12 = 5e-5           # mass-squared of nu_1 (assumed)
+    m22 = m12 + dm2         # mass-squared of nu_2
 
     # Function definitions
 
     # Bahcall standard solar approx. Eq. (14) in Bahcall et al
     # Ap J 555, 990 (2001)
 
-    aa = 1.47539e26;
-    alph = 10.54;
-
-    n = 1000;
-    x = 0.0:0.01:n;
-
-    ne(x) = aa*exp(-alph*x);
+    aa = 1.47539e26
+    alph = 10.54
 
     # Resonance length in meters
-    norm=9.785e30;
-    scriptell_m(x) = norm/ne(x);
+    norm = 9.785e30
 
     # Critical (resonance) density
-
-    neR = 3.95e30*(dm2/E)*cos(2*tv);
-
+    neR = 3.95e30 * (dm2 / E) * cos(2.0 * tv)
     #  Radius of resonance density with Bahcall param
     #  in fraction of solar radius
-
-    rcrit = -(1/alph)*log(neR/aa);
-
+    rcrit = -(1.0 / alph) * log(neR / aa)
     # Vacuum oscillation length in meters
-
-    L = 2.48*E/dm2;
-
+    L = 2.48 * E / dm2
     # Matter mixing angle
+    C = cos(2.0 * tv)
+    S = sin(2.0 * tv)
 
-    C = cos(2*tv);
-    S = sin(2*tv);
+    # Make vector, not matrix
+    n = 1000
+    x = collect(range(0.01,0.6,n));
+    thetam = zeros(n)
+    # Main loop
+    i = 1;
+    for i = 1:n
+        xi = x[i]
+        ne = aa * exp(-alph * xi)
+        scriptell_m = norm / ne
+        chi = L/scriptell_m
+        sin2tm = S / sqrt((C - chi)^2 + S^2)
 
-    chi(x) = L/scriptell_m(x);
+        # Matter mixing angle in radians
+        thetam[i] = 0.5 * asin(sin2tm)
+        # Evaluate matter angle in correct angle sector for x=R/R0
+        thetam[i] = (xi > rcrit) ? thetam[i] : pi / 2 - thetam[i]  # Below and above resonance
+        print(i,",",xi,",",thetam[i],"\n")
+        i += 1
+    end
 
-    sin2tm(x) = S/sqrt( (C-chi(x))^2 +S^2 );
+    layout = Layout(; title = "Matter Mixing Angle Like Gnuplot")
+    plot(x, thetam, layout)
 
-    # Matter mixing angle in radians
-
-    thetam(x) = 0.5*asin(sin2tm(x));
-
-    # Evaluate matter angle in correct angle sector for x=R/R0
-
-    tm(x) = x > rcrit ? thetam(x) : pi/2-thetam(x);  # Below and above resonance
-
-    layout = Layout(;title="Matter Mixing Angle Like Gnuplot");
-    println("$x,$tm");
-    plot(x,tm,layout);
 end
 
 function plotCouplingStrength()
 
-    values[100];
+    values[100]
     for i = 1:100
         #values[i] = #getCouplingStrength(i*neutrinos.energy,)
     end
